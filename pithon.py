4db0f8b8b09133ff70c1ecffb6c74b0f9f9ffc03
Odilbek <odilbekmirzarakhimov@gmail.com>
2026-06-09 20:23:58 +0500

    fix event loop

--- pithon.py
+++ pithon.py
@@ -109,4 +109,9 @@ async def main():
 
 # ТОЧКА ВХОДА (С правильными отступами)
 if __name__ == '__main__':
-    main()
+    import asyncio
+    try:
+        asyncio.run(main())
+    except RuntimeError:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
